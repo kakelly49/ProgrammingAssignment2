6f4eb20d5f5615a56501857f13bb011163e53f10
kakelly <kakelly@findhelp.ca>
2017-02-19 23:46:03 -0500

    Programming Assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,32 @@
-## Put comments here that give an overall description of what your
+## These functions read a square matrix 
 ## functions do
 
-## Write a short comment describing this function
+## The makeCacheMatrix function returns a list consisting of x,i (inverse of x) and 
+## the 4 functions set,get,setinverse and getinverse 
 
 makeCacheMatrix <- function(x = matrix()) {
+        #check to see if input x has an inverse   
+        #if not,exit function    
+        ##if(det(x) == 0){ 
+	   ##   stop("Singular matrix - no inverse"
+          ##}
+        i <- NULL      
+        set <- function(y) {
+	     x<<-y
+           m <<- NULL
+       }
+        get <- function() x
+        setinverse <- function(solve) i <<- solve
+        getinverse <- function() i
+        list(set = set, get = get,
+              setinverse = setinverse,
+              getinverse = getinverse)	
+ }
 
-}
 
-
-## Write a short comment describing this function
+## The cacheSolve function 
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+
 }
